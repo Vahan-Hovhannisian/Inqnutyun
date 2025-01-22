f01c447262b47507a68962323ef724aa98440c41
Shant <shant212205@gmail.com>
2025-01-22 05:58:31 -0800

    global chenge

--- script.js
+++ script.js
@@ -1,50 +1,4 @@
-/**********************--------------*****************/
-let copyright = document.getElementById("copyright");
-copyright.innerHTML = new Date().getFullYear();
-/************* */
 
-const form = document.getElementById('form');
-const result = document.getElementById('result');
-
-if (form) {
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        const formData = new FormData(form);
-        const object = Object.fromEntries(formData);
-        const json = JSON.stringify(object);
-        result.innerHTML = "Please wait..."
-
-        fetch('https://api.web3forms.com/submit', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: json
-        })
-            .then(async (response) => {
-                let json = await response.json();
-                if (response.status == 200) {
-                    result.innerHTML = "Form submitted successfully";
-                } else {
-                    console.log(response);
-                    result.innerHTML = json.message;
-                }
-            })
-            .catch(error => {
-                console.log(error);
-                result.innerHTML = "Something went wrong!";
-            })
-            .then(function () {
-                form.reset();
-                setTimeout(() => {
-                    result.style.display = "none";
-                }, 3000);
-            });
-    });
-}
-
-/*********************************/
 
 function myMenuFunction() {
 
@@ -154,21 +108,25 @@ if (coinsBtn && idramBtn) {
     });
 }
 
-
-var TrandingSlider = new Swiper('.tranding-slider', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    loop: true,
-    slidesPerView: 'auto',
-    coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 100,
-        modifier: 2.5,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
+if (document.querySelector(".mySwiper")) {
+    const swiper = new Swiper(".mySwiper", {
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        }, slidesPerView: 3,
+        breakpoints: {
+            0: {
+                slidesPerView: 1, 
+                spaceBetween : 20
+            },
+            576: {
+                slidesPerView: 2,
+                spaceBetween : 80
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween : 80
+            }
+        },
+    });
+}
