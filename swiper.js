bdfe83b2962f8055790a3204d36b1a55b88076b4
Shant <shant212205@gmail.com>
2025-01-23 07:18:10 -0800

    update

--- swiper.js
+++ swiper.js
@@ -1,227 +1,77 @@
+const slider = document.querySelector(".slider");
+const sliderContainer = document.querySelector(".slider-container");
+const slides = document.querySelectorAll(".slide");
+const sliderNavigations = document.querySelectorAll(".slider-navigation");
+const actionPrevious = document.querySelector(".action-prev");
+const actionNext = document.querySelector(".action-next");
 
-const repeat = false;
-const noArrows = false;
-const noBullets = false;
+let activeOrder = 0;
 
+init();
 
-const container = document.querySelector('.slider-container');
-var slide = document.querySelectorAll('.slider-single');
-var slideTotal = slide.length - 1;
-var slideCurrent = -1;
-
-function initBullets() {
-    if (noBullets) {
-        return;
+function init() {
+    for (let i = 0; i < slides.length; i++) {
+        const slide = slides[i];
+        slide.dataset.order = i;
+        slide.style.transform = `translate(-50%, -50%)`;
+        slide.addEventListener("click", clickHandler);
     }
-    const bulletContainer = document.createElement('div');
-    bulletContainer.classList.add('bullet-container')
-    slide.forEach((elem, i) => {
-        const bullet = document.createElement('div');
-        bullet.classList.add('bullet')
-        bullet.id = `bullet-index-${i}`
-        bullet.addEventListener('click', () => {
-            goToIndexSlide(i);
-        })
-        bulletContainer.appendChild(bullet);
-        elem.classList.add('proactivede');
-    })
-    container.appendChild(bulletContainer);
-}
-
-function initArrows() {
-    if (noArrows) {
-        return;
+    for (const navigation of sliderNavigations) {
+        navigation.addEventListener("click", navigationHandler);
     }
-    const leftArrow = document.createElement('a')
-    const iLeft = document.createElement('i');
-    iLeft.classList.add('fa')
-    iLeft.classList.add('fa-arrow-left')
-    leftArrow.classList.add('slider-left')
-    leftArrow.appendChild(iLeft)
-    leftArrow.addEventListener('click', () => {
-        slideLeft();
-    })
-    const rightArrow = document.createElement('a')
-    const iRight = document.createElement('i');
-    iRight.classList.add('fa')
-    iRight.classList.add('fa-arrow-right')
-    rightArrow.classList.add('slider-right')
-    rightArrow.appendChild(iRight)
-    rightArrow.addEventListener('click', () => {
-        slideRight();
-    })
-    container.appendChild(leftArrow);
-    container.appendChild(rightArrow);
+
+    activeOrder = Math.floor(slides.length / 2);
+
+    update();
 }
 
-function slideInitial() {
-    initBullets();
-    initArrows();
-    setTimeout(function () {
-        slideRight();
-    }, 500);
-}
+function update() {
+    const { width, height } = sliderContainer.getBoundingClientRect();
+    //const slideRect = slides[0].getBoundingClientRect();
+    const a = width / 2;
+    const b = height / 2;
 
-function updateBullet() {
-    if (!noBullets) {
-        document.querySelector('.bullet-container').querySelectorAll('.bullet').forEach((elem, i) => {
-            elem.classList.remove('active');
-            if (i === slideCurrent) {
-                elem.classList.add('active');
-            }
-        })
-    }
-    checkRepeat();
-}
+    const delta = Math.PI / slides.length / 4;
 
-function checkRepeat() {
-    if (!repeat) {
-        if (slideCurrent === slide.length - 1) {
-            slide[0].classList.add('not-visible');
-            slide[slide.length - 1].classList.remove('not-visible');
-            if (!noArrows) {
-                document.querySelector('.slider-right').classList.add('not-visible')
-                document.querySelector('.slider-left').classList.remove('not-visible')
-            }
+    for (let i = 0; i < slides.length; i++) {
+        const leftSlide = document.querySelector(
+            `[data-order="${activeOrder - i}"]`,
+        );
+        if (leftSlide) {
+            leftSlide.style.zIndex = slides.length - i;
+            leftSlide.style.opacity = 1 - (4 * i) / slides.length;
+            leftSlide.style.left = `${width / 2 + a * Math.cos((Math.PI * 3) / 2 - delta * i * 3)}px`;
+            leftSlide.style.top = `${-b * Math.sin((Math.PI * 3) / 2 - delta * i * 2)}px`;
+            leftSlide.style.scale = 1 - (1 * i) / slides.length;
         }
-        else if (slideCurrent === 0) {
-            slide[slide.length - 1].classList.add('not-visible');
-            slide[0].classList.remove('not-visible');
-            if (!noArrows) {
-                document.querySelector('.slider-left').classList.add('not-visible')
-                document.querySelector('.slider-right').classList.remove('not-visible')
-            }
-        } else {
-            slide[slide.length - 1].classList.remove('not-visible');
-            slide[0].classList.remove('not-visible');
-            if (!noArrows) {
-                document.querySelector('.slider-left').classList.remove('not-visible')
-                document.querySelector('.slider-right').classList.remove('not-visible')
-            }
+        const rightSlide = document.querySelector(
+            `[data-order="${activeOrder + i}"]`,
+        );
+        if (rightSlide) {
+            rightSlide.style.zIndex = slides.length - i;
+            rightSlide.style.opacity = 1 - (4 * i) / slides.length;
+            rightSlide.style.left = `${width / 2 + a * Math.cos((Math.PI * 3) / 2 + delta * i * 2)}px`;
+            rightSlide.style.top = `${-b * Math.sin((Math.PI * 3) / 2 + delta * i * 2)}px`;
+            rightSlide.style.scale = 1 - (1 * i) / slides.length;
         }
     }
 }
 
-function slideRight() {
-    if (slideCurrent < slideTotal) {
-        slideCurrent++;
-    } else {
-        slideCurrent = 0;
-    }
-
-    if (slideCurrent > 0) {
-        var preactiveSlide = slide[slideCurrent - 1];
-    } else {
-        var preactiveSlide = slide[slideTotal];
-    }
-    var activeSlide = slide[slideCurrent];
-    if (slideCurrent < slideTotal) {
-        var proactiveSlide = slide[slideCurrent + 1];
-    } else {
-        var proactiveSlide = slide[0];
-
-    }
-
-    slide.forEach((elem) => {
-        var thisSlide = elem;
-        if (thisSlide.classList.contains('preactivede')) {
-            thisSlide.classList.remove('preactivede');
-            thisSlide.classList.remove('preactive');
-            thisSlide.classList.remove('active');
-            thisSlide.classList.remove('proactive');
-            thisSlide.classList.add('proactivede');
-        }
-        if (thisSlide.classList.contains('preactive')) {
-            thisSlide.classList.remove('preactive');
-            thisSlide.classList.remove('active');
-            thisSlide.classList.remove('proactive');
-            thisSlide.classList.remove('proactivede');
-            thisSlide.classList.add('preactivede');
-        }
-    });
-    preactiveSlide.classList.remove('preactivede');
-    preactiveSlide.classList.remove('active');
-    preactiveSlide.classList.remove('proactive');
-    preactiveSlide.classList.remove('proactivede');
-    preactiveSlide.classList.add('preactive');
-
-    activeSlide.classList.remove('preactivede');
-    activeSlide.classList.remove('preactive');
-    activeSlide.classList.remove('proactive');
-    activeSlide.classList.remove('proactivede');
-    activeSlide.classList.add('active');
-
-    proactiveSlide.classList.remove('preactivede');
-    proactiveSlide.classList.remove('preactive');
-    proactiveSlide.classList.remove('active');
-    proactiveSlide.classList.remove('proactivede');
-    proactiveSlide.classList.add('proactive');
-
-    updateBullet();
+function clickHandler() {
+    activeOrder = parseInt(this.dataset.order, 10);
+    update();
 }
 
-function slideLeft() {
-    if (slideCurrent > 0) {
-        slideCurrent--;
-    } else {
-        slideCurrent = slideTotal;
+function navigationHandler(e) {
+    e.preventDefault();
+    const { dir } = this.dataset;
+
+    if (dir === "prev") {
+        activeOrder = Math.max(0, activeOrder - 1);
+    } else if (dir === "next") {
+        activeOrder = Math.min(slides.length - 1, activeOrder + 1);
     }
-
-    if (slideCurrent < slideTotal) {
-        var proactiveSlide = slide[slideCurrent + 1];
-    } else {
-        var proactiveSlide = slide[0];
-    }
-    var activeSlide = slide[slideCurrent];
-    if (slideCurrent > 0) {
-        var preactiveSlide = slide[slideCurrent - 1];
-    } else {
-        var preactiveSlide = slide[slideTotal];
-    }
-    slide.forEach((elem) => {
-        var thisSlide = elem;
-        if (thisSlide.classList.contains('proactive')) {
-            thisSlide.classList.remove('preactivede');
-            thisSlide.classList.remove('preactive');
-            thisSlide.classList.remove('active');
-            thisSlide.classList.remove('proactive');
-            thisSlide.classList.add('proactivede');
-        }
-        if (thisSlide.classList.contains('proactivede')) {
-            thisSlide.classList.remove('preactive');
-            thisSlide.classList.remove('active');
-            thisSlide.classList.remove('proactive');
-            thisSlide.classList.remove('proactivede');
-            thisSlide.classList.add('preactivede');
-        }
-    });
-
-    preactiveSlide.classList.remove('preactivede');
-    preactiveSlide.classList.remove('active');
-    preactiveSlide.classList.remove('proactive');
-    preactiveSlide.classList.remove('proactivede');
-    preactiveSlide.classList.add('preactive');
-
-    activeSlide.classList.remove('preactivede');
-    activeSlide.classList.remove('preactive');
-    activeSlide.classList.remove('proactive');
-    activeSlide.classList.remove('proactivede');
-    activeSlide.classList.add('active');
-
-    proactiveSlide.classList.remove('preactivede');
-    proactiveSlide.classList.remove('preactive');
-    proactiveSlide.classList.remove('active');
-    proactiveSlide.classList.remove('proactivede');
-    proactiveSlide.classList.add('proactive');
-
-    updateBullet();
+    update();
 }
 
-function goToIndexSlide(index) {
-    const sliding = (slideCurrent > index) ? () => slideRight() : () => slideLeft();
-    while (slideCurrent !== index) {
-        sliding();
-    }
-}
-
-slideInitial();
+//window.addEventListener("resize", update);
